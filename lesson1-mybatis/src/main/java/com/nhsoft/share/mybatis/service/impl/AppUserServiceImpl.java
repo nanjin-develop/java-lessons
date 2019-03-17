c0cd97396a9f83af5549e6a6c2a6481016b0c7da
卢苏东 <nhsoft.lsd@hotmail.com>
2019-03-17 15:14:40 +0800

    images

--- lesson1-mybatis/src/main/java/com/nhsoft/share/mybatis/service/impl/AppUserServiceImpl.java
+++ lesson1-mybatis/src/main/java/com/nhsoft/share/mybatis/service/impl/AppUserServiceImpl.java
@@ -4,15 +4,22 @@ package com.nhsoft.share.mybatis.service.impl;
 import com.nhsoft.share.mybatis.dao.AppUserDao;
 import com.nhsoft.share.mybatis.model.AppUser;
 import com.nhsoft.share.mybatis.service.AppUserService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @Transactional
 public class AppUserServiceImpl implements AppUserService {
+
+    @Autowired
+    private SqlSessionFactory sqlSessionFactory;
 
     @Autowired
     private AppUserDao appUserDao;
@@ -36,7 +43,28 @@ public class AppUserServiceImpl implements AppUserService {
 
     @Override
     public List<AppUser> list() {
-        appUserDao.list();
-        return appUserDao.list();
+
+        SqlSession sqlSession1 = sqlSessionFactory.openSession(true);
+        AppUserDao appUserDao1 = sqlSession1.getMapper(AppUserDao.class);
+
+        log.info("我开启啦session1************************\r\n");
+
+
+        appUserDao1.list();
+//        sqlSession1.commit();
+
+        log.info("第一次查询************************\r\n" );
+
+        SqlSession sqlSession2 = sqlSessionFactory.openSession(true);
+        AppUserDao appUserDao2 = sqlSession2.getMapper(AppUserDao.class);
+
+        log.info("我开启啦session2************************\r\n");
+
+        List<AppUser> list = appUserDao2.list();
+
+        log.info("第二次查询************************\r\n");
+        sqlSession2.commit();
+
+        return list;
     }
 }
