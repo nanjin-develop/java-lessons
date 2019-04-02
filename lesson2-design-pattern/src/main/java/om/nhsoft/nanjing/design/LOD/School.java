0ad9d024d9eb1ac56218526edb839c8f169192d7
卢苏东 <nhsoft.lsd@hotmail.com>
2019-04-02 17:28:33 +0800

    设计模式-最少知道原则

---
+++ lesson2-design-pattern/src/main/java/om/nhsoft/nanjing/design/LOD/School.java
@@ -0,0 +1,17 @@
+package om.nhsoft.nanjing.design.LOD;
+
+/**
+ * @Author: nhsoft.lsd
+ * @Description:
+ * @Date:Create：in 2019-04-02 17:23
+ * @Modified By：
+ */
+public class School {
+
+    private String name;
+
+
+    public void print(){
+        // TODO 实现
+    }
+}
---
+++ lesson2-design-pattern/src/main/java/om/nhsoft/nanjing/design/LOD/Student.java
@@ -0,0 +1,17 @@
+package om.nhsoft.nanjing.design.LOD;
+
+import lombok.Data;
+
+/**
+ * @Author: nhsoft.lsd
+ * @Description:
+ * @Date:Create：in 2019-04-02 17:23
+ * @Modified By：
+ */
+@Data
+public class Student {
+
+    private String id;
+    private String name;
+
+}
